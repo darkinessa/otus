b5615dea6acf954ec3515cc7fccbf5eaa1937af6
Irina Koroleva <korolevai@gmail.com>
2020-02-24 19:10:08 +0300

    added markup templates

--- blog_engine/app/view.py
+++ blog_engine/app/view.py
@@ -5,4 +5,22 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hi!'
+    return render_template('public/index.html')
+
+
+@app.route('/static-template')
+def static_template():
+    return render_template('public/static-template.html')
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template('public/contacts.html')
+
+@app.route('/post')
+def post():
+    return render_template('public/post.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('public/blog.html')
